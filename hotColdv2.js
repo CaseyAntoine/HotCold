c4b79e08e6a47881dc6df56112a0ec946131d8e5
Casey <crantoine@gmail.com>
2013-09-11 21:05:58 -0400

    Updated JS/jQuery

--- hotColdv2.js
+++ hotColdv2.js
@@ -1,4 +1,4 @@
-var randomNum = Math.floor(Math.random() * 101);
+/* var randomNum = Math.floor(Math.random() * 101);
 var diff = 0, 
 lastDiff = 0,
 userGuess = document.getElementById('guessInput').value;
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
 function logPastDiff(userGuess){
 	diff = Math.abs(userGuess - randomNum);
-	
+
 	if(userGuess != randomNum){
 		lastDiff = diff;
 	}
@@ -25,4 +25,78 @@ function getNumber(userGuess){
 
 
 	}
-};
+}; */
+
+(function () {
+    "use strict";
+    var randomNumber = Math.floor(Math.random() * 101),
+        lastDiff = 0,
+        diff = 0;
+    
+    var guessNumber = function(userNumber){
+        
+        if(userNumber == randomNumber){
+    
+            $('#message').text('Hey! You got it!');
+            break;
+        }        
+        
+        diff = Math.abs(randomNumber - userNumber);
+        if(isNaN(userNumber)){
+            $('#message').text('That\'s not a number.');
+            }
+        else if(diff < lastDiff){
+            if(diff < 10){
+                $('#message').text('Getting so Hot, Try again!');
+                document.getElementById('#gamebox').addClass('.highHot');
+            }
+            else if(diff < 15 && diff > 10){
+                $('#message').text('Getting much Warmer, Try again!'); 
+            }else{
+                $('#message').text('Getting Warmer, Try again!');             
+            }
+    
+        }else{
+            if(diff == lastDiff){
+                $('#message').text('You already tried that');
+            }
+            else if(diff > 30){
+                $('#message').text('Getting Much Colder, Try again!');
+                document.getElementById('#gameBox').addClass('.highCold');
+            }else{
+                $('#message').text('Getting Colder, Try again!');             
+    
+            } 
+        }
+        lastDiff = diff;
+        
+        return diff;
+    }
+
+
+    //
+    // DOCUMENT READY
+    //
+    $(document).ready(function () {
+        var userNumber;
+        
+        $("#submit").onclick(function(){
+        
+            userNumber = $('#userNumber').val();
+            diff = guessNumber(userNumber);
+         });
+        // Accept Enter within input
+        $("#guessInput").keyup(function(){
+            if(event.keyCode == 13){
+            $("#submit"); 
+            }
+        });
+        /* $("#reset").click(function(){
+            location.reload();
+        }); */	
+        
+        $("#lastGuess").text(diff);
+
+    });
+
+} ());
